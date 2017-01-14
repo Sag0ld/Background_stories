ae596b94d060fb9692d24e2f7058377b69c8fc08
Sagold <daphneepatenaude@gmail.com>
2017-01-13 21:04:21 -0500

    ajout notification nouveau fond+ Set and crop

--- app/src/main/java/com/sag0ld/background_stories/MainActivity.java
+++ app/src/main/java/com/sag0ld/background_stories/MainActivity.java
@@ -1,21 +1,31 @@
 package com.sag0ld.background_stories;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.app.WallpaperManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Calendar;
 
 public class MainActivity extends Activity {
@@ -70,15 +80,71 @@ public class MainActivity extends Activity {
                 new FindWallpaper().execute(pathFolder);
                 spinner.setVisibility(View.GONE);
                 btnDone.setEnabled(true);
+                Toast bread = Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG);
+                bread.show();
             }
         };
-
         // Initialize action
         buttonFindFolder.setOnClickListener(findFolderListener);
         btnBrowsePicture.setOnClickListener(browsePictureListener);
         btnDone.setOnClickListener(btnDoneOnClick);
+
+        // Notification
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.folder)
+                        .setContentTitle("New Wallpaper found!")
+                        .setContentText("We find a new match!");
+
+        // Creates an explicit intent for an Activity in your app
+        WallpaperManager myWallpaperManager
+                = WallpaperManager.getInstance(getApplicationContext());
+        File wallpaperFile = new File("/storage/emulated/0/Pictures/Messenger/received_1330987716952430.jpeg");
+        Uri contentURI = getImageContentUri(this, wallpaperFile.getAbsolutePath());
+        Intent intent = myWallpaperManager.getCropAndSetWallpaperIntent(contentURI);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
+
+       /* AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 00);
+        calendar.set(Calendar.SECOND, 00);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24*60*60*1000 ,
+        resultPendingIntent);  //set repeating every 24 hours*/
     }
 
+    // http://stackoverflow.com/questions/23207604/get-a-content-uri-from-a-file-uri
+    public Uri getImageContentUri(Context context, String absPath) {
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                , new String[]{MediaStore.Images.Media._ID}
+                , MediaStore.Images.Media.DATA + "=? "
+                , new String[]{absPath}, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            cursor.close();
+            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
+
+        } else if (!absPath.isEmpty()) {
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.DATA, absPath);
+            return context.getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        } else {
+            return null;
+        }
+    }
     @Override
     public void onResume() {
         super.onResume();
@@ -142,15 +208,16 @@ public class MainActivity extends Activity {
             }
             return null;
         }
-
+        @Override
         protected void onProgressUpdate(Integer... progress) {
             spinner.setProgress(progress[0]);
         }
+        @Override
         protected void onPreExecute() {
-            spinner.setVisibility(View.VISIBLE);
+            spinner.setVisibility(Spinner.VISIBLE);
             btnDone.setEnabled(false);
         }
-
+        @Override
         protected void onPostExecute(Bitmap result) {
             WallpaperManager myWallpaperManager
                     = WallpaperManager.getInstance(getApplicationContext());
@@ -165,7 +232,6 @@ public class MainActivity extends Activity {
                 // Set a wallpaper by default
                 try {
                     File defaultPicture = new File(pathPicture);
-
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),
                             android.net.Uri.parse(defaultPicture.toURI().toString()));
                     myWallpaperManager.setBitmap(bitmap);
